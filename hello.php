a67d82bb08ac72cc3d19ee5c1114a6694debad7c
az007a <60051173+az007a@users.noreply.github.com>
2020-04-20 06:38:22 -0500

    Insert course

--- hello.php
+++ hello.php
@@ -3,9 +3,11 @@
 <h3>Main Menu</h3>
 
 <form action="hello.php" method="post">
-	<input type = "submit" class = "button" name = "init" value = "initDatabase" /> <br> <br><br>  
+	<input type = "submit" class = "button" name = "init" value = "initDatabase" /> <br> <br><br>
 	 <a href = "http://www.csce.uark.edu/~xsguo/project_cpp/odbc_insert_restaurant.php"> Insert Student </a>
 	<br><br>
+	<a href = "http://www.csce.uark.edu/~xsguo/project_cpp/odbc_insert_course.php"> Insert Course </a>
+  <br><br>
 	<a href = "http://www.csce.uark.edu/~xsguo/project_cpp/odbc_view_student.php"> View All Students </a>
 	<br><br>
 	<a href = "http://www.csce.uark.edu/~xsguo/project_cpp/odbc_view_course.php"> View Course from Department </a>
@@ -20,7 +22,7 @@
 <?php
 if (isset($_POST['init'])) {
 	$command = '/home/xsguo/public_html/project_cpp/hello.exe';
-	
+
 	$command = escapeshellcmd($command);
 
 	echo "command: $command <br>";
